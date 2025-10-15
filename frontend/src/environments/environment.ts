186b83a015a377fa357cb9aa8a4291285a8e0874
m4cx <macgyverx83@gmail.com>
2025-10-15 15:38:48 +0200

    fix(otlp-frontend): fix endpoints for frontend tracing

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  otlpEndpoint: 'http://jaeger:6060/v1/traces'
+  otlpEndpoint: 'http://localhost:6060/v1/traces'
 };
